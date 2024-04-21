baf1c242ae33080ced4cbe28c6012a9e2d94701e
Sifan23 <sifanyg123@gmail.com>
2024-04-21 21:26:40 +0300

    forexchart component

---
+++ packages/react-app/components/ForexChart.tsx
@@ -0,0 +1,50 @@
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      label: "USD to EUR",
+      data: [1.1, 1.2, 1.3, 1.4, 1.5, 1.6],
+      borderColor: "rgb(75, 192, 192)",
+      backgroundColor: "rgba(75, 192, 192, 0.5)",
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Monthly Exchange Rate",
+    },
+  },
+};
+
+export default function ForexChart() {
+  return <Line data={data} options={options} />;
+}
